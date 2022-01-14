fc356d931caac30a0e92088b1f36cceb08adfac8
澁谷悠大 <univer405@gmail.com>
2022-01-14 23:56:37 +0900

    TodoCLI完成

--- target/debug/notion-cli.d
+++ target/debug/notion-cli.d
@@ -1,1 +1,1 @@
-/Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/target/debug/notion-cli: /Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/src/cli.rs /Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/src/main.rs
+/Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/target/debug/notion-cli: /Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/src/cli.rs /Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/src/main.rs /Users/shibuyayudai/src/github.com/shibuya-yudai/notion-cli/src/tasks.rs
